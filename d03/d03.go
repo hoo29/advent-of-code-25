49bea5f9f9f84b1551d9156b248112974266c4a2
Huw McNamara <huwmcnamara@msn.com>
2025-12-22 08:45:41 +0000

    d3

--- d03/d03.go
+++ d03/d03.go
@@ -2,10 +2,9 @@ package main
 
 import (
 	"aoc/utils"
-	"fmt"
 	"log"
 	"math"
-	"sync"
+	"strings"
 	"time"
 )
 
@@ -34,38 +33,16 @@ func p1(data []string) {
 	log.Printf("p1 %v", p1)
 }
 
-func p2(data string, onCount int, ind int, sum int, maxOnCount int, cache map[string]int, maxCache map[int]int) int {
-
-	if onCount == maxOnCount {
-		return sum
+func findMax(data []int, start int, end int) (int, int) {
+	max := -1
+	ind := -1
+	for i := start; i < end; i++ {
+		if data[i] > max {
+			max = data[i]
+			ind = i
+		}
 	}
-
-	if ind == len(data) {
-		return -1
-	}
-
-	if onCount+(len(data)-ind) < maxOnCount {
-		return -1
-	}
-
-	if maxCache[onCount] > sum {
-		return -1
-	}
-
-	key := fmt.Sprintf("%v_%v_%v", onCount, sum, ind)
-	if val, ok := cache[key]; ok {
-		// log.Println("cache hit")
-		return val
-	}
-
-	sum1 := sum + utils.Atoi(string(data[ind]))*int(math.Pow10(maxOnCount-onCount-1))
-
-	val1 := p2(data, onCount+1, ind+1, sum1, maxOnCount, cache, maxCache)
-	val2 := p2(data, onCount, ind+1, sum, maxOnCount, cache, maxCache)
-	maxVal := max(val1, val2)
-	cache[key] = maxVal
-	maxCache[onCount] = maxVal
-	return maxVal
+	return max, ind
 }
 
 func main() {
@@ -78,28 +55,29 @@ func main() {
 	}
 
 	start := time.Now()
-	// p1(data)
-	// log.Printf("p1 took %s", time.Since(start))
+	p1(data)
+	log.Printf("p1 took %s", time.Since(start))
 
 	start = time.Now()
 	total := 0
-	var wg sync.WaitGroup
-	var mut sync.Mutex
-	for i, line := range data {
-		wg.Add(1)
-		go func(l string) {
-			log.Printf("%v/%v", i+1, len(data))
-			cache := make(map[string]int)
-			maxCache := make(map[int]int)
-
-			res := p2(l, 0, 0, 0, 12, cache, maxCache)
-			log.Printf("max %v", res)
-			mut.Lock()
-			total += res
-			mut.Unlock()
-		}(line)
+	maxOn := 12
+	for _, line := range data {
+		ints := make([]int, len(line))
+		for ind, c := range strings.Split(line, "") {
+			ints[ind] = utils.Atoi(c)
+		}
+		startInd := 0
+		lineAns := 0
+		for onCount := range maxOn {
+			endInd := len(ints) - (maxOn - onCount - 1)
+			val, valInd := findMax(ints, startInd, endInd)
+			lineAns += val * int(math.Pow10(maxOn-onCount-1))
+			startInd = valInd + 1
+		}
+		total += lineAns
+		// log.Printf("line %v", line)
+		// log.Printf("max %v", lineAns)
 	}
-	wg.Wait()
 
 	log.Printf("p2 %v", total)
 	log.Printf("p2 took %s", time.Since(start))
