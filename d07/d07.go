54ba3a04b2797916955a7a41237e79d53cdb2538
Huw McNamara <huwmcnamara@msn.com>
2025-12-22 18:00:39 +0000

    d07

--- d07/d07.go
+++ d07/d07.go
@@ -53,10 +53,41 @@ func p1(data []string) {
 	log.Printf("p1 ans %v", ans)
 }
 
-func p2(data []string) {
+func dfs(data []string, pos []int, cache map[string]int) int {
+	if pos[1] >= len(data) {
+		return 1
+	}
+	line := data[pos[1]]
+	if pos[0] < 0 || pos[0] >= len(line) {
+		return 0
+	}
+	key := fmt.Sprintf("%v_%v", pos[0], pos[1])
+	if cacheVal, ok := cache[key]; ok {
+		return cacheVal
+	}
 
+	char := utils.GetRuneFromString(line, pos[0])
+	sum := 0
+	if char == '.' {
+		sum += dfs(data, []int{pos[0], pos[1] + 1}, cache)
+	} else {
+		sum += dfs(data, []int{pos[0] - 1, pos[1]}, cache)
+		sum += dfs(data, []int{pos[0] + 1, pos[1]}, cache)
+	}
+	cache[key] = sum
+	return sum
+}
+
+func p2(data []string) {
 	start := time.Now()
-	ans := 0
+	var startInd int
+	for ind, c := range data[0] {
+		if c == 'S' {
+			startInd = ind
+			break
+		}
+	}
+	ans := dfs(data, []int{startInd, 1}, map[string]int{})
 
 	log.Printf("p2 took %s", time.Since(start))
 	log.Printf("p2 ans %v", ans)
